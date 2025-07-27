0a41ba60ed4a4a785ec75636327bab3e0159a47a
Mxqwthl <sc15933332964@gmail.com>
2025-07-28 06:54:00 +0800

    yolo

--- scripts/dataset_convert.py
+++ scripts/dataset_convert.py
@@ -6,8 +6,6 @@ import argparse
 
 def convert_dataset(src_dir, dest_dir):
     # 构建目标数据集目录结构
-    if os.path.exists(dest_dir):
-        shutil.rmtree(dest_dir)
     os.makedirs(dest_dir)
     os.makedirs(path.join(dest_dir, "train", "images"), exist_ok=True)
     os.makedirs(path.join(dest_dir, "train", "labels"), exist_ok=True)
